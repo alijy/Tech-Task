a1250f6f06468c5fd0396d86c9628c1364c40b1b
Ali Karami <karamiali19@gmail.com>
2018-09-05 17:44:25 +0100

    added the third approach using model and view

--- src/main/java/com/alikarami/employees/Main.java
+++ src/main/java/com/alikarami/employees/Main.java
@@ -1,6 +1,8 @@
 package com.alikarami.employees;
 
+import spark.ModelAndView;
 import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,16 +14,24 @@ public class Main {
 
     public static void main(String[] args) {
 
+        /* first approach */
+//        get("/person", (request, response) -> "name: ali");
 
+
+        /* second approach */
 //        get("/person", (request, response) -> {
-//            Map<String, String> model = new HashMap<>();
-//            modelAndView(model, "/person");
-//            return "Another Person";
+//            response.type("application/json");
+//            return "{\"name\":\"ali\" , \"age\":\"36\"}";
 //        });
 
+        /* third approach */
         get("/person", (request, response) -> {
-            response.type("application/json");
-            return "{\"name\":\"ali\" , \"gender\":\"male\"}";
+            Map<String, Object> model = new HashMap<>();
+            model.put("name","ali");
+            model.put("age","36");
+            return new HandlebarsTemplateEngine().render(
+                    new ModelAndView(model, "person.hbs")
+            );
         });
 
 
